188ca4e50fffc4e624907d605f68d6e65117e4e8
Yoo-Nah Park <ypark2270@gmail.com>
2017-03-06 18:02:56 -0500

    install supertest and supertest-as-promised; start answers route test

---
+++ server/routes/answers.test.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+const db = require('../../db');
+const Answer = require('../../db/models/answer');
+const app = require('../start');
+
+describe('/api/answers', () => {
+
+})
